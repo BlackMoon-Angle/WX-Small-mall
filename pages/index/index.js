c3b772b4be3b3037041c03903d49890c326d443d
BlackMoon-Angle <2717773566@qq.com>
2021-02-20 14:03:19 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,47 @@
+//Page Object
+Page({
+  data: {
+    // 轮播图数组
+    swiperList:[]
+  },
+  // 页面开始加载就会触发
+  onLoad: function(options){
+    // 发送异步请求——获取轮播图数据
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result)=>{
+        this.setData({
+          swiperList:result.data.message
+        })
+      }
+    });
+  },
+  onReady: function(){
+    
+  },
+  onShow: function(){
+    
+  },
+  onHide: function(){
+
+  },
+  onUnload: function(){
+
+  },
+  onPullDownRefresh: function(){
+
+  },
+  onReachBottom: function(){
+
+  },
+  onShareAppMessage: function(){
+
+  },
+  onPageScroll: function(){
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item){
+
+  }
+});
