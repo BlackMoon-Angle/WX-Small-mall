645232d9e6cbd7059ea3f9a89dfeb8751bcd93f8
BlackMoon-Angle <2717773566@qq.com>
2021-02-21 14:10:19 +0800

    首页完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,12 +6,15 @@ Page({
     // 轮播图数组
     swiperList:[],
     // 导航数组
-    cateList: []
+    cateList: [],
+    // 楼层数据
+    floorList:[]
   },
   // 页面开始加载就会触发
   onLoad: function(options){
     this.getSwiperList()
     this.getCateList()
+    this.getfloorList()
   },
   // 发送异步请求——获取 轮播图 数据 
   getSwiperList(){
@@ -30,6 +33,15 @@ Page({
     }).then(result => {
         this.setData({
           cateList:result.data.message
+        })
+    })
+  },
+  getfloorList(){
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
+    }).then(result => {
+        this.setData({
+          floorList:result.data.message
         })
     })
   },
