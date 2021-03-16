b3000860752364ac6c190657eaf48560b7d4425c
BlackMoon-Angle <2717773566@qq.com>
2021-03-16 15:49:36 +0800

    个人中心，商品收藏

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -7,7 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    goodsObj:{}
+    goodsObj:{},
+    isCollect: false // 商品是否被收藏
   },
   // 商品对象
   GoodsInfo: {},
@@ -15,7 +16,10 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onShow: function () {
+    let pages = getCurrentPages();
+    let currentPage = pages[pages.length - 1]
+    let options = currentPage.options
     const { goods_id } = options
     this.getGoodsDetail(goods_id)
   },
@@ -23,6 +27,10 @@ Page({
   async getGoodsDetail(goods_id){
     const res = await request({ url: '/goods/detail', data: { goods_id } })
     this.GoodsInfo = res.data.message
+    // 获取缓存中的商品收藏数组
+    let collect = wx.getStorageSync('collect') || [];
+    // 判断当前商品是否被收藏
+    let isCollect = collect.some(v => v.goods_id === this.GoodsInfo.goods_id)
     this.setData({
       // 优化动态渲染，去掉请求回来数据中不需要的子数据，仅提出需要数据
       goodsObj: {
@@ -33,7 +41,8 @@ Page({
         // .webp => .jpg
         goods_introduce: res.data.message.goods_introduce.replace(/\.webp/g, '.jpg'),
         pics: res.data.message.pics
-      }
+      },
+      isCollect
     })
   },
   // 点击轮播图，预览大图
@@ -71,5 +80,39 @@ Page({
       icon: 'success',
       mask: true // 防止用户反复点击按钮
     });
+  },
+  // 点击商品收藏
+  handleCollect() {
+    let isCollect = false
+    // 获取缓存中的商品收藏数组
+    let collect = wx.getStorageSync('collect') || [];
+    // 判断该商品是否被收藏过
+    let index = collect.findIndex(v => v.goods_id === this.GoodsInfo.goods_id)
+    // 当 index != -1 ，说明已经收藏了
+    if(index !== -1) {
+      // 找到 已经收藏的商品，在数组中进行删除
+      collect.splice(index, 1)
+      isCollect = false
+      wx.showToast({
+        title: '取消收藏！',
+        icon: 'success',
+        mask: true
+      });
+    } else {
+      // 没有收藏的情况
+      collect.push(this.GoodsInfo)
+      isCollect = true
+      wx.showToast({
+        title: '收藏成功！',
+        icon: 'success',
+        mask: true
+      });
+    }
+    // 把数组存入缓存中
+    wx.setStorageSync('collect', collect);
+    // 修改 data 中 isCollect 的属性
+    this.setData({
+      isCollect
+    })
   }
 })
--- pages/order/index.js
+++ pages/order/index.js
@@ -1,66 +1,83 @@
 // pages/order/index.js
+import {  } from '../../utils/asyncWX.js'
+import { request } from "../../request/index.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: '全部',
+        isActive: true
+      },
+      {
+        id: 1,
+        value: '待付款',
+        isActive: false
+      },
+      {
+        id: 2,
+        value: '待发货',
+        isActive: false
+      },
+      {
+        id: 2,
+        value: '退款/退货',
+        isActive: false
+      }
+    ],
+    orders: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 标题点击事件，从子组件传递参数过来
+  handleTabsItemChange(e){
+    // 获取被点击的标题索引
+    const { index } = e.detail
+    this.changeTitleByIndex(index)
+    // 重新发送请求
+    this.getOrder(index + 1)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 根据标题索引来激活选中的标题
+  changeTitleByIndex(index) {
+    // 修改源数组
+    const { tabs } = this.data
+    tabs.forEach((val, i) => {
+      i === index ? val.isActive = true : val.isActive = false
+    })
+    this.setData({
+      tabs
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  onShow: function() {
+    const token = wx.getStorageSync('token');
+    if(!token) {
+      wx.navigateTo({
+        url: '/pages/auth/index'
+      });
+      return
+    }
+    // 获取当前小程序的页面栈-数组，长度最大为10个页面
+    let pages = getCurrentPages();
+    // 数组中 索引最大的页面就是当前页
+    let { options: { type } } = pages[pages.length - 1]
+    // 激活选中的页面标题
+    this.changeTitleByIndex(type - 1)
+    this.getOrder(type)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取订单列表的方法
+  async getOrder(type) {
+    const { data: { message: { orders } } } = await request({
+      url: '/my/orders/all',
+      data: { type }
+    })
+    // 时间错转化
+    this.setData({
+      orders: orders.map(v => ({
+        ...v,
+        create_time_cn: (new Date(v.create_time * 1000).toLocaleString())
+      }))
+    })
   }
 })
